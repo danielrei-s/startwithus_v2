3fff8b1758426eb82cd730d861f411703df1c7d9
Filipe Ferreira <45075634+H2OAguas@users.noreply.github.com>
2023-02-09 15:33:07 +0000

    With Inserts

--- StartWithUsV2/DBScript.sql
+++ StartWithUsV2/DBScript.sql
@@ -16,6 +16,20 @@ status INT(1) NOT NULL DEFAULT 0,
 nif VARCHAR(255) NOT NULL,
 PRIMARY KEY (idUser)
 );
+INSERT INTO `users` (`idType`, `firstName`, `lastName`, `birthdayDate`, `genre`, `email`, `token`, `emailValidation`, `password`, `dateCreated`, `dateModified`, `status`, `nif`) VALUES
+(2, 'Filipe', 'Ferreira', '1987-06-12', 'M', 'filipecbr@gmail.com', 'c4b0df2812', 1, '1111', '2023-02-07 11:40:06', NULL, 1, '25545884112');
+INSERT INTO `users` (`idType`, `firstName`, `lastName`, `birthdayDate`, `genre`, `email`, `token`, `emailValidation`, `password`, `dateCreated`, `dateModified`, `status`, `nif`) VALUES
+(2, 'Abel', 'Araujo', '1987-06-12', 'M', 'abel.araujo@startwithus.com', 'c4b0df2812', 1, '1111', '2023-02-07 11:40:06', NULL, 1, '25545284112');
+INSERT INTO `users` (`idType`, `firstName`, `lastName`, `birthdayDate`, `genre`, `email`, `token`, `emailValidation`, `password`, `dateCreated`, `dateModified`, `status`, `nif`) VALUES
+(2, 'Alberta', 'Rodrigues', '1977-06-12', 'F', 'alberta.rodrigues@startwithus.com', 'c4b0df2812', 1, '1111', '2023-02-07 11:40:06', NULL, 1, '25445884112');
+INSERT INTO `users` (`idType`, `firstName`, `lastName`, `birthdayDate`, `genre`, `email`, `token`, `emailValidation`, `password`, `dateCreated`, `dateModified`, `status`, `nif`) VALUES
+(2, 'Owner1', 'Owner1', '1977-06-12', 'F', 'owner1@startwithus.com', 'c4b0df2812', 1, '1111', '2023-02-07 11:40:06', NULL, 1, '25445884112');
+INSERT INTO `users` (`idType`, `firstName`, `lastName`, `birthdayDate`, `genre`, `email`, `token`, `emailValidation`, `password`, `dateCreated`, `dateModified`, `status`, `nif`) VALUES
+(2, 'Owner2', 'Owner2', '1977-06-12', 'F', 'owner2@startwithus.com', 'c4b0df2812', 1, '1111', '2023-02-07 11:40:06', NULL, 1, '25445884112');
+INSERT INTO `users` (`idType`, `firstName`, `lastName`, `birthdayDate`, `genre`, `email`, `token`, `emailValidation`, `password`, `dateCreated`, `dateModified`, `status`, `nif`) VALUES
+(2, 'Angel1', 'Angel1', '1977-06-12', 'M', 'angel1@startwithus.com', 'c4b0df2812', 1, '1111', '2023-02-07 11:40:06', NULL, 1, '25445884112');
+INSERT INTO `users` (`idType`, `firstName`, `lastName`, `birthdayDate`, `genre`, `email`, `token`, `emailValidation`, `password`, `dateCreated`, `dateModified`, `status`, `nif`) VALUES
+(2, 'Angel2', 'Angel2', '1977-06-12', 'M', 'angel2@startwithus.com', 'c4b0df2812', 1, '1111', '2023-02-07 11:40:06', NULL, 1, '25445884112');
 CREATE TABLE Projects (
 idProject INT NOT NULL AUTO_INCREMENT,
 idOwner INT NOT NULL,
@@ -27,6 +41,9 @@ expertNeeds VARCHAR(255) NOT NULL,
 PRIMARY KEY (idProject),
 FOREIGN KEY (idOwner) REFERENCES Users(idUser)
 );
+INSERT INTO `projects`(`idOwner`, `initialDate`, `summaryDescription`, `extendedDescription`, `raiseObjective`, `expertNeeds`) VALUES ('9','2023-02-07 11:40:06','Descrição Sumária do Projeto do Owner 1','Descrição extensiva do projeto Owner 1]','10000','N');
+INSERT INTO `projects`(`idOwner`, `initialDate`, `summaryDescription`, `extendedDescription`, `raiseObjective`, `expertNeeds`) VALUES ('9','2023-02-07 11:40:06','Descrição Sumária do segundo Projeto Owner 1','Descrição extensiva do segundo projeto Owner 1','20000','N');
+INSERT INTO `projects`(`idOwner`, `initialDate`, `summaryDescription`, `extendedDescription`, `raiseObjective`, `expertNeeds`) VALUES ('10','2023-02-07 11:40:06','Descrição Sumária do Projeto do Owner 2','Descrição extensiva do projeto do Owner 2','35000','N');
 CREATE TABLE Projects_Comparticipation (
 id INT NOT NULL AUTO_INCREMENT,
 idProject INT NOT NULL,
@@ -36,6 +53,8 @@ PRIMARY KEY (id),
 FOREIGN KEY (idProject) REFERENCES Projects(idProject),
 FOREIGN KEY (idUser) REFERENCES Users(idUser)
 );
+INSERT INTO `projects_comparticipation`(`idProject`, `idUser`, `value`) VALUES ('2','11','2000');
+
 CREATE TABLE Projects_FullReaders (
 idProject INT NOT NULL,
 idUser INT NOT NULL,
@@ -44,6 +63,11 @@ PRIMARY KEY (idProject,idUser),
 FOREIGN KEY (idProject) REFERENCES Projects(idProject),
 FOREIGN KEY (idUser) REFERENCES Users(idUser)
 );
+INSERT INTO `projects_fullreaders`(`idProject`, `idUser`, `FinalDate`) VALUES ('2','11','2023-04-07 11:40:06');
+INSERT INTO `projects_fullreaders`(`idProject`, `idUser`, `FinalDate`) VALUES ('3','11','2023-04-07 11:40:06');
+
+
+
 CREATE TABLE Experts (
 id INT NOT NULL AUTO_INCREMENT,
 idProject INT NOT NULL,
@@ -54,6 +78,7 @@ extendedDescription TEXT NOT NULL,
 PRIMARY KEY (id),
 FOREIGN KEY (idProject) REFERENCES Projects(idProject)
 );
+INSERT INTO `experts`(`idProject`, `idExpert`, `expertTitle`, `smallDescription`, `extendedDescription`) VALUES ('2','2','WebDesigner','Criação de Site estático','Criação de site estático com 4 páginas');
 CREATE TABLE Messages (
 id INT NOT NULL AUTO_INCREMENT,
 idProject INT NOT NULL,
@@ -73,5 +98,9 @@ FOREIGN KEY (idProject) REFERENCES Projects(idProject),
 FOREIGN KEY (idSender) REFERENCES Users(idUser),
 FOREIGN KEY (idReceiver) REFERENCES Users(idUser)
 );
+INSERT INTO `messages`(`idProject`, `idAction`, `idSender`, `idReceiver`, `sendDate`, `type`, `proposalValue`, `proposalPercentage`, `counterProposalValue`, `counterProposalPercentage`, `Accept`, `PaymentConfirmed`) VALUES ('2','11','11','9','2023-01-07 11:40:06','','1000','5','','','0','0');
+INSERT INTO `messages`(`idProject`, `idAction`, `idSender`, `idReceiver`, `sendDate`, `type`, `proposalValue`, `proposalPercentage`, `counterProposalValue`, `counterProposalPercentage`, `Accept`, `PaymentConfirmed`) VALUES ('2','9','11','11','2023-01-010 11:40:06','','1000','5','2000','5','0','0');
+INSERT INTO `messages`(`idProject`, `idAction`, `idSender`, `idReceiver`, `sendDate`, `type`, `proposalValue`, `proposalPercentage`, `counterProposalValue`, `counterProposalPercentage`, `Accept`, `PaymentConfirmed`) VALUES ('2','11','11','9','2023-01-15 11:40:06','','2000','5','','','1','1');
+INSERT INTO `messages`(`idProject`, `idAction`, `idSender`, `idReceiver`, `sendDate`, `type`, `proposalValue`, `proposalPercentage`, `counterProposalValue`, `counterProposalPercentage`, `Accept`, `PaymentConfirmed`) VALUES ('3','11','11','10','2023-01-15 11:40:06','','2000','5','','','1','1');
 
 
