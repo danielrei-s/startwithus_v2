5d5ca1fa3f806d56f2c8edfe50dbea7515667ad6
Filipe Ferreira <45075634+H2OAguas@users.noreply.github.com>
2023-02-10 19:06:14 +0000

    Gravarmsg

--- StartWithUsV2/mensagens.php
+++ StartWithUsV2/mensagens.php
@@ -2,9 +2,12 @@
 session_start();
 include ("connect.php");
 $idUser = $_SESSION["id"];
-$idProjeto= $_GET["idProjeto"];
+// $idProjeto= $_GET["idProjeto"];
+$idProject= "2";
 
-$owner= $_GET["owner"];
+
+// $owner= $_GET["owner"];
+$owner= "4";
 
 $result = mysqli_query($conn,"SELECT * FROM users WHERE idUser = $idUser");
   $row = mysqli_fetch_assoc($result);
@@ -91,57 +94,75 @@ $result = mysqli_query($conn,"SELECT * FROM users WHERE idUser = $idUser");
 
 
     <?php
-      if($owner == "n")
+      if($owner == "4")
       {
-
-   
+        $projectsOwners =  mysqli_query($conn, "SELECT Distinct idOwner FROM Projects WHERE idProject = $idProject");
+        $projectsOwner = mysqli_fetch_assoc($projectsOwners);
+        $projOwner = $projectsOwner["idOwner"];
+        $comIteractions = mysqli_query($conn,"SELECT distinct * FROM messages WHERE (idSender = $idUser or idSender = $projOwner) and (idReceiver = $projOwner or idReceiver = $idUser) ");
+        $comIteraction = mysqli_fetch_assoc($comIteractions);
+        if($comIteraction){
+          $idAction= $comIteraction["idAction"]  ;
+        }else{
+          $getMaxidActions = mysqli_query($conn,"SELECT * FROM messages ORDER BY idAction DESC LIMIT 0, 1");
+          $getMaxidAction = mysqli_fetch_assoc($getMaxidActions);        
+          $idAction=  $getMaxidAction["idAction"] + 1;
+        }
+        $idSender= $idUser;
+        $idReceiver= $projectsOwner["idOwner"];
+        $sendDate= "";
+        $mFlag= "0";
+        $proposalValue= "0";
+        $proposalPercentage= "0";
+        $counterProposalValue= "0";
+        $counterProposalPercentage= "0";
+        $Accept= "0";
+        $PaymentConfirmed= "0";
     ?> 
-
+</br></br></br></br></br></br></br>
     <form action="gravamensagem.php" method="post">
       <label for="id"style="display:none;">ID:</label>
       <input type="hidden" id="id" name="id">
 
       <label for="idProject"style="display:none;">ID Project:</label>
-      <input type="hidden" id="idProject" name="idProject">
+      <input type="hidden" id="idProject" name="idProject" value="<?php  echo $idProject ?>">
 
       <label for="idAction"style="display:none;">ID Action:</label>
-      <input type="hidden" id="idAction" name="idAction">
+      <input type="hidden" id="idAction" name="idAction" value="<?php  echo $idAction ?>">
 
       <label for="idSender"style="display:none;">ID Sender:</label>
-      <input type="hidden" id="idSender" name="idSender">
+      <input type="hidden" id="idSender" name="idSender" value="<?php  echo $idSender ?>">
 
-      <label for="senderName"style="display:none;">Nome Emissor:</label>
-      <input type="hidden" id="idSender" name="<?php echo $userFirstName ?>">
+      <!-- <label for="senderName"style="display:none;">Nome Emissor:</label>
+      <input type="hidden" id="idSender" name="<?php echo $userFirstName ?>"> -->
 
       <label for="idReceiver"style="display:none;">ID Receiver:</label>
-      <input type="hidden" id="idReceiver" name="idReceiver">
+      <input type="hidden" id="idReceiver" name="idReceiver" value="<?php  echo $idReceiver ?>">
 
       <label for="sendDate">Data de Envio:</label>
-      <input type="text" id="sendDate" name="sendDate" value="<?php echo date('d-m-Y H:i:s'); ?>"><br><br>
+      <input type="text" id="sendDate" name="sendDate" size="10" value="<?php echo date('d-m-Y H:i:s'); ?>"><br><br>
 
 
-      <label for="type"style="display:none;">Type:</label>
-      <input type="hidden" id="type" name="type">
+      <label for="type"style="display:none;">mFlag:</label>
+      <input type="hidden" id="type" name="mFlag" value="<?php  echo $mFlag ?>">
 
       <label for="proposalValue">Valor da proposta:</label>
-      <input type="number" id="proposalValue" name="proposalValue" step="0.01"><br><br>
+      <input type="number" id="proposalValue" name="proposalValue"  value="<?php  echo $proposalValue ?>" step="0.01"><br><br>
 
 
       <label for="proposalPercentage">Percentagem pretendida:</label>
-      <input type="number" id="proposalPercentage" name="proposalPercentage" step="0.01"><br><br>
+      <input type="number" id="proposalPercentage" name="proposalPercentage"  value="<?php  echo $proposalPercentage ?>" step="0.01"><br><br>
 
       <label for="counterProposalValue"style="display:none;">Counter Proposal Value:</label>
-      <input type="hidden" id="counterProposalValue" name="counterProposalValue">
+      <input type="hidden" id="counterProposalValue" name="counterProposalValue" value="<?php  echo $counterProposalValue ?>">
 
       <label for="counterProposalPercentage"style="display:none;">Counter Proposal Percentage:</label>
-      <input type="hidden" id="counterProposalPercentage" name="counterProposalPercentage">
-
-      <button type="button" onclick="AcceptProposal()">Accept Proposal</button><br><br>
+      <input type="hidden" id="counterProposalPercentage" name="counterProposalPercentage" value="<?php  echo $counterProposalPercentage ?>">
 
       <label for="PaymentConfirmed"style="display:none;">Payment Confirmed:</label>
-      <input type="hidden" id="PaymentConfirmed" name="PaymentConfirmed"><br><br>
-
-      <input type="button" value="Enviar">
+      <input type="hidden" id="PaymentConfirmed" name="PaymentConfirmed" value="<?php  echo $PaymentConfirmed ?>"><br><br>
+      <input type="image" name="submit" border="0" src="assets/img/sendButton.png" alt="Send"> 
+      
       </form>
   
           <?php
